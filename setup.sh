8e27967815ea323d229605b86277204a345131ef
Benjamin Altieri <benaltieri@gmail.com>
2018-06-18 16:04:02 -0700

    History stuff; No hard tabs and add branch name aliases in gitconfig

--- setup.sh
+++ setup.sh
@@ -84,6 +84,22 @@ else
 	echo "WARNING: Can't find virtualenvwrapper.sh"
 fi
 codeblock
+# save all bash history
+cat >> $BASHRC <<'codeblock'
+# Eternal bash history.
+# ---------------------
+# Undocumented feature which sets the size to "unlimited".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=-1
+export HISTSIZE=-1
+export HISTTIMEFORMAT="[%F %T] "
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+codeblock
 source $BASH_PROFILE
 virtualenvwrapper
 
